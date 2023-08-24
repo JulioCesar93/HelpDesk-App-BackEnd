7c32158851bad77c86a8849ddac920c94db244f5
julio_m3_@hotmail.com <julio_m3_@hotmail.com>
2023-08-23 22:13:32 -0300

    Profile test created

---
+++ src/main/java/com/jcs/helpdesk/services/DBService.java
@@ -0,0 +1,39 @@
+package com.jcs.helpdesk.services;
+
+import com.jcs.helpdesk.domain.Cliente;
+import com.jcs.helpdesk.domain.Tecnico;
+import com.jcs.helpdesk.domain.Ticket;
+import com.jcs.helpdesk.domain.enums.Perfil;
+import com.jcs.helpdesk.domain.enums.Prioridade;
+import com.jcs.helpdesk.domain.enums.Status;
+import com.jcs.helpdesk.repositories.ClienteRepository;
+import com.jcs.helpdesk.repositories.TecnicoRepository;
+import com.jcs.helpdesk.repositories.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class DBService {
+
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    public void instanciaDB() {
+        Tecnico tec1 = new Tecnico(null, "Lemes Gustavo", "52584633035", "Lemesgus@mail.com", "alg78");
+        tec1.addPerfil(Perfil.ADMIN);
+
+        Cliente cli1 = new Cliente(null, "Amanda Fernandes", "45082546082", "amandafer261102@mail.com", "banco01");
+
+        Ticket sr1 = new Ticket(null, Prioridade.MEDIA, Status.ANDAMENTO, "SR-01", "Primeiro Ticket", tec1, cli1);
+
+        tecnicoRepository.saveAll(Arrays.asList(tec1));
+        clienteRepository.saveAll(Arrays.asList(cli1));
+        ticketRepository.saveAll(Arrays.asList(sr1));
+    }
+}
