e2d3384b0da4f4475e97c28f186e3231c20b33c3
julio_m3_@hotmail.com <julio_m3_@hotmail.com>
2023-09-13 10:42:57 -0300

    Inserted findAll Tickets

--- src/main/java/com/jcs/helpdesk/services/TicketService.java
+++ src/main/java/com/jcs/helpdesk/services/TicketService.java
@@ -6,6 +6,7 @@ import com.jcs.helpdesk.services.exceptions.ObjectnotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,9 @@ public class TicketService {
     public Ticket findById(Integer id) {
         Optional<Ticket> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectnotFoundException("Ticket não encontrado! OS: " + id));
+    }
+
+    public List<Ticket> findAll() {
+        return repository.findAll();
     }
 }
